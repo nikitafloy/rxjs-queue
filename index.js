bed315762a5a1304151a597b3a97b3be674df092
nikitafloy <nikita5051@yandex.ru>
2023-05-12 15:29:44 +0700

    add priority

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { find, firstValueFrom, Subject } from "rxjs";
+import { filter, firstValueFrom, Subject } from "rxjs";
 import { v4 as uuidv4 } from "uuid";
 
 /* Создать логику, при обращении к одной и той же функции с одним ключом,
@@ -9,148 +9,136 @@ const subject$ = new Subject();
 const keys = {};
 
 const enter = async (key, taskId, priority = 255) => {
+  const timestamp = Number(new Date());
+
   // В очереди никого, сразу входим в очередь и выполняем промис
   if (!keys[key]) {
-    keys[key] = [{ id: taskId, priority }];
+    keys[key] = [{ id: taskId, timestamp, priority }];
     return;
   }
 
   // В очереди кто-то есть, занимаем очередь
 
   // Занимаем очередь
-  keys[key].push({ taskId, priority });
+  keys[key].push({ id: taskId, timestamp, priority });
 
   // Сортируем по приоритету, может мы имеем привелегию в очереди
   // Сравним свой приоритет с другими
-  const sortedByPriority = keys[key].sort((a, b) => {
+  // Обновим текущую очередь по приоритету
+  keys[key].sort((a, b) => {
+    if (a.priority === b.priority) {
+      return a.timestamp - b.timestamp;
+    }
+
     return a.priority - b.priority;
   });
 
-  // Обновим текущую очередь по приоритету
-  for (let index = 0; index < keys[key].length; index++) {
-    keys[key][index] = {
-      pos: index + 1,
-      priority: sortedByPriority[index].priority,
-    };
-  }
-
-  // keys[key] = sortedByPriority;
-
-  // За кем мы в очереди в итоге?
-
-  // Ищем себя
-  const me = keys[key].find((item) => item.priority >= priority);
-
-  console.log(priority, me);
-
-  await wait(taskId, prev);
-
-  return taskId;
+  await wait(key, taskId);
 };
 
-const quit = (key, pos) => {
-  keys[key] = keys[key].filter((item) => item.pos !== pos);
+const quit = (key, taskId) => {
+  keys[key] = keys[key].filter((item) => item.id !== taskId);
 
   subject$.next(key);
 };
 
-const wait = (key, prev) => {
-  return firstValueFrom(
+const wait = async (key, taskId) => {
+  return await firstValueFrom(
     subject$.pipe(
-      find((value) => {
-        console.log(keys[value], "===", prev.pos);
+      filter((value) => {
+        if (key !== value) {
+          return;
+        }
 
-        return value === key && (!keys[value] || keys[value].pos === prev.pos);
+        if (!keys[value]) {
+          return;
+        }
+
+        if (keys[value][0].id !== taskId) {
+          return;
+        }
+
+        return true;
       })
     )
   );
 };
 
-const resolvePromise = async (promise, key, priority) => {
+const resolvePromise = async (key, name, args, priority = 255) => {
   const taskId = uuidv4();
 
   await enter(key, taskId, priority);
+
+  const promise = getPromiseByName(key, name, Object.values(args));
 
   const r = await promise;
 
   quit(key, taskId);
 
-  console.log(keys[key]);
-
   return r;
+};
+
+const getPromiseByName = (key, name, args) => {
+  switch (name) {
+    case "fetchNumber":
+      return fetchNumber(...args);
+  }
+};
+
+const fetchNumber = (number, executionTime = 300) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      console.log(number);
+      res(number);
+    }, executionTime);
+  });
 };
 
 try {
   console.log("Start");
 
-  // setTimeout(() => quit(1), 2000);
-  // setTimeout(() => quit(1), 1000);
-  // setTimeout(() => quit(1), 100);
-
   const result = await Promise.all([
     resolvePromise(
-      new Promise((res, rej) => {
-        setTimeout(() => {
-          console.log("1");
-          res("1");
-        }, 800);
-      }),
-      "get number"
-    ),
-    resolvePromise(
-      new Promise((res, rej) => {
-        setTimeout(() => {
-          console.log("2");
-          res("2");
-        }, 1000);
-      }),
-      "get number"
-    ),
-    resolvePromise(
-      new Promise((res, rej) => {
-        setTimeout(() => {
-          console.log("3");
-          res("3");
-        }, 300);
-      }),
-      "get number"
-    ),
-    resolvePromise(
-      new Promise((res, rej) => {
-        setTimeout(() => {
-          console.log("4");
-          res("4");
-        }, 3000);
-      }),
-      "get number"
-    ),
-    resolvePromise(
-      new Promise((res, rej) => {
-        setTimeout(() => {
-          console.log("5");
-          res("5");
-        }, 3200);
-      }),
       "get number",
+      "fetchNumber",
+      { number: 1, executionTime: 300 },
+      255
+    ),
+    resolvePromise(
+      "get number",
+      "fetchNumber",
+      { number: 2, executionTime: 1000 },
+      255
+    ),
+    resolvePromise(
+      "get number",
+      "fetchNumber",
+      { number: 3, executionTime: 300 },
+      100
+    ),
+    resolvePromise(
+      "get number",
+      "fetchNumber",
+      { number: 4, executionTime: 3000 },
+      200
+    ),
+    resolvePromise(
+      "get number",
+      "fetchNumber",
+      { number: 5, executionTime: 3200 },
       0
     ),
     resolvePromise(
-      new Promise((res, rej) => {
-        setTimeout(() => {
-          console.log("6");
-          res("6");
-        }, 300);
-      }),
-      "get number"
+      "get number",
+      "fetchNumber",
+      { number: 6, executionTime: 300 },
+      255
     ),
     resolvePromise(
-      new Promise((res, rej) => {
-        setTimeout(() => {
-          console.log("7");
-          res("7");
-        }, 300);
-      }),
-      "get number"
+      "get number",
+      "fetchNumber",
+      { number: 7, executionTime: 300 },
+      255
     ),
   ]);
 
